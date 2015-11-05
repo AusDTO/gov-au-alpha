d8e5d19ade09ca633240043b63d6cb93d2ceded8
Nat Young <nat.young7@gmail.com>
2015-11-05 16:19:49 +1100

    basic workflow and persist HTTP GET vars

--- js/app.js
+++ js/app.js
@@ -3,3 +3,32 @@ function getParameterByName(name) {
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+var DTO = DTO || {};
+
+DTO.Forms = (function(window, undefined) {
+  var extractHttpGetParams = function() {
+    var match,
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function (s) {
+        return decodeURIComponent(s.replace( /\+/g, " "));
+      },
+      query = window.location.search.substring(1);
+    var urlParams = {};
+    while (match = search.exec(query)) {
+      urlParams[decode(match[1])] = decode(match[2]);
+    }
+    return urlParams;
+  };
+
+  var persistHttpGetParams = function() {
+    var params = extractHttpGetParams();
+    for(var key in params) {
+      $('form').append('<input type="hidden" id="' + key + '" name="' + key + '" value="' + params[key] + '" />')
+    }
+  };
+
+  return {
+    persistHttpGetParams : persistHttpGetParams
+  }
+})(window);
